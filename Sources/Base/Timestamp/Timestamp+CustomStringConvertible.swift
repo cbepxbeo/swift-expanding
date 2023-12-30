4c5e2c6edec92ba4390a86a17eb21e3b33b3e46a
cbepxbeo <cbepxbeo@mail.ru>
2023-12-30 16:33:16 +0300

    Update Timestamp+CustomStringConvertible.swift

--- Sources/Base/Timestamp/Timestamp+CustomStringConvertible.swift
+++ Sources/Base/Timestamp/Timestamp+CustomStringConvertible.swift
@@ -15,7 +15,7 @@
  |  File: Timestamp+CustomStringConvertible.swift
  |  Created by: Egor Boyko
  |  Date: December 29th, 2023
- |  Last update: December 29th, 2023
+ |  Last update: December 30th, 2023
  |  Version: 0.0.1
  |---------------------------------------------------------------------------------------
  |  Status: #In progress | #Not decorated
@@ -23,8 +23,42 @@
  
  */
 
+#if canImport(Darwin)
+import Darwin
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(WinSDK)
+import WinSDK
+#endif
+
 extension Timestamp: CustomStringConvertible {
     public var description: String {
-        "Timestamp (seconds: \(self.rawValue.0), nanoseconds: \(self.rawValue.1))"
+#if canImport(Glibc)
+        let format = "%4Y-%m-%d %H:%M:%S +0000"
+#else
+        let format = "%Y-%m-%d %H:%M:%S +0000"
+#endif
+        let unavailable = "<description unavailable>"
+        guard self >= Self.distant(.past),  self <= Self.distant(.future) else {
+            return unavailable
+        }
+        var info = tm()
+#if os(Windows)
+        var time = __time64_t(self.receive(option: .timeIntervalSince1970))
+        let errno: errno_t = _gmtime64_s(&info, &time)
+        guard errno == 0 else { return unavailable }
+#else
+        var time = time_t(self.receive(option: .timeIntervalSince1970))
+        gmtime_r(&time, &info)
+#endif
+        let bufferSize = 128
+        return withUnsafeTemporaryAllocation(of: CChar.self, capacity: bufferSize) { buffer in
+            guard let ptr = buffer.baseAddress,
+                  strftime(ptr, bufferSize, format, &info) != 0,
+                  let result = String(validatingUTF8: ptr) else {
+                return unavailable
+            }
+            return result
+        }
     }
 }
