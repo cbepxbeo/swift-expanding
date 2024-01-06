31c1fd5fc506f4301796c5a50d9a7b4301390c48
Egor Olegovich <cbepxbeo@mail.ru>
2024-01-06 20:47:26 +0300

    Create Matrixed+Implementation+Hashable.swift

---
+++ Sources/Matrixed/Matrixed+Implementation+Hashable.swift
@@ -0,0 +1,31 @@
+/*
+ 
+ |---------------------------------------------------------------------------------------
+ |  Copyright The Swift-Expanding Contributors.
+ |  Licensed under the Apache License, Version 2.0 (the "License");
+ |  you may not use this file except in compliance with the License.
+ |  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ |
+ |  Unless required by applicable law or agreed to in writing, software
+ |  distributed under the License is distributed on an "AS IS" BASIS,
+ |  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ |  See the License for the specific language governing permissions and
+ |  limitations under the License.
+ |---------------------------------------------------------------------------------------
+ |  File: Matrixed+Implementation+Hashable.swift
+ |  Created by: Egor Boyko
+ |  Date: January 6th, 2024
+ |  Last update: January 6th, 2024
+ |  Version: 0.0.1
+ |---------------------------------------------------------------------------------------
+ |  Status: #In progress | #Not decorated
+ |---------------------------------------------------------------------------------------
+ 
+ */
+
+extension Matrixed where Element: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        self.storage.hash(into: &hasher)
+    }
+}
+
