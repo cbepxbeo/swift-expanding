a728e84cfbb019494c25c11a7ac061bd75144ce3
cbepxbeo <cbepxbeo@mail.ru>
2024-01-07 17:25:16 +0300

    Update Matrix+Method+Set.swift
    Updated logic to not get index out of range
    Add docs

--- Sources/Base/Matrix/Matrix+Method+Set.swift
+++ Sources/Base/Matrix/Matrix+Method+Set.swift
@@ -18,15 +18,35 @@
  |  Last update: January 7th, 2024
  |  Version: 0.0.1
  |---------------------------------------------------------------------------------------
- |  Status: #In progress | #Not decorated
+ |  Status: #Completed | #Decorated
  |---------------------------------------------------------------------------------------
  
  */
 
 extension Matrix {
+    /// Sets a new value at the specified coordinates
+    ///
+    ///     var matrix = try! Matrix(column: 3, row: 3, defaultValue: 0)
+    ///
+    ///     //Row 1:          | 0 | 0 | 0 |
+    ///     //Row 2:          | 0 | 0 | 0 |
+    ///     //Row 3:          | 0 | 0 | 0 |
+    ///
+    ///     try? matrix.set(5, x: 2, y: 3)
+    ///
+    ///     //Row 1:          | 0 | 0 | 0 |
+    ///     //Row 2:          | 0 | 0 | 0 |
+    ///     //Row 3:          | 0 | 5 | 0 |
+    ///
+    /// - Parameter value: New value
+    /// - Parameter x: Coordinate corresponding to the position in the line
+    /// - Parameter y: Coordinate corresponding to the position in the column
+    /// - Note: By calling a method in different threads on the same instance, you will not receive an exception, but the behavior will be undefined
     public mutating func set(_ value: Element, x xCoordinate: Int, y yCoordinate: Int) throws {
-        let index = try self.index(x: xCoordinate, y: yCoordinate)
-        self.storage[index] = value
+        var temp = self
+        let index = try temp.index(x: xCoordinate, y: yCoordinate)
+        temp.storage[index] = value
+        self = temp
     }
 }
 
