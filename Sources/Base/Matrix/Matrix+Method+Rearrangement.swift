9edf8a2c268071b84728e78122e34f15fae1978f
Egor Olegovich <cbepxbeo@mail.ru>
2024-01-10 23:49:41 +0300

    Create Matrix+Method+Rearrangement.swift

---
+++ Sources/Base/Matrix/Matrix+Method+Rearrangement.swift
@@ -0,0 +1,35 @@
+/*
+ 
+ |---------------------------------------------------------------------------------------
+ |  Copyright The Swift-Expanding Contributors.
+ |  Licensed under the Apache License, Version 2.0 (the "License");
+ |  you may not use this file except in compliance with the License.
+ |  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ |
+ |  Unless required by applicable law or agreed to in writing, software
+ |  distributed under the License is distributed on an "AS IS" BASIS,
+ |  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ |  See the License for the specific language governing permissions and
+ |  limitations under the License.
+ |---------------------------------------------------------------------------------------
+ |  File: Matrix+Method+Rearrangement.swift
+ |  Created by: Egor Boyko
+ |  Date: January 10th, 2024
+ |  Last update: January 10th, 2024
+ |  Version: 0.0.1
+ |---------------------------------------------------------------------------------------
+ |  Status: #In progress | #Not decorated
+ |---------------------------------------------------------------------------------------
+ 
+ */
+
+extension Matrix {
+    public mutating func rearrangement(from: Int, to: Int) -> Bool{
+        if self.storage[from] != nil {
+            self.storage[to] = self.storage[from]
+            self.storage[from] = nil
+            return true
+        }
+        return false
+    }
+}
