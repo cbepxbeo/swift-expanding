eb3d4b7a79b9851c6f553c851ea1da145b31b639
Egor Olegovich <cbepxbeo@mail.ru>
2024-01-13 14:39:48 +0300

    Update Matrix+Method+SumIdentical.swift
    More correct method responsibility

--- Sources/Base/Matrix/Matrix+Method+SumIdentical.swift
+++ Sources/Base/Matrix/Matrix+Method+SumIdentical.swift
@@ -12,7 +12,7 @@
  |  See the License for the specific language governing permissions and
  |  limitations under the License.
  |---------------------------------------------------------------------------------------
- |  File: Matrix+Method+SummationIdentical.swift
+ |  File: Matrix+Method+SumIdentical.swift
  |  Created by: Egor Boyko
  |  Date: January 11th, 2024
  |  Last update: January 13th, 2024
@@ -24,55 +24,53 @@
  */
 
 extension Matrix where Element: SignedNumeric {
-    ///Moves all elements according to the direction and sums those identical ones that have the same direction.
+    ///Sums identical elements that are in adjacent cells in the specified direction
     ///
-    ///Example with moving elements up
+    ///Example with upward direction
     ///
-    ///     //Row 1:          | nil |  1  | nil | nil |
-    ///     //Row 2:          | nil |  1  |  2  | nil |
-    ///     //Row 3:          | nil | nil | nil | nil |
-    ///     //Row 4:          | nil |  2  |  2  | nil |
-    ///     //Row 4:          | nil |  1  | nil | nil |
+    ///     //Row 1:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 2:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 3:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 4:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 5:          | 2 | 2 | 2 | 2 | 2 |
     ///
-    ///     matrix.summationIdentical(.upward)
+    ///     matrix.sumIdentical(in: .upward)
     ///
-    ///     //Row 1:          | nil |  4  |  4  | nil |
-    ///     //Row 2:          | nil |  1  | nil | nil |
-    ///     //Row 3:          | nil | nil | nil | nil |
-    ///     //Row 4:          | nil | nil | nil | nil |
-    ///     //Row 4:          | nil | nil | nil | nil |
+    ///     //Row 1:          |  4  |  4  |  4  |  4  |  4  |
+    ///     //Row 2:          | nil | nil | nil | nil | nil |
+    ///     //Row 3:          |  4  |  4  |  4  |  4  |  4  |
+    ///     //Row 4:          | nil | nil | nil | nil | nil |
+    ///     //Row 5:          |  2  |  2  |  2  |  2  |  2  |
     ///
-    ///Example with moving elements left
+    ///Example with left direction
     ///
-    ///     //Row 1:          | nil |  1  | nil | nil |
-    ///     //Row 2:          | nil |  1  |  2  | nil |
-    ///     //Row 3:          | nil | nil | nil | nil |
-    ///     //Row 4:          | nil |  2  |  2  | nil |
-    ///     //Row 4:          | nil |  1  | nil | nil |
+    ///     //Row 1:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 2:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 3:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 4:          | 2 | 2 | 2 | 2 | 2 |
+    ///     //Row 5:          | 2 | 2 | 2 | 2 | 2 |
     ///
-    ///     matrix.summationIdentical(.left)
+    ///     matrix.sumIdentical(in: .left)
     ///
-    ///     //Row 1:          |  1  | nil | nil | nil |
-    ///     //Row 2:          |  1  |  2  | nil | nil |
-    ///     //Row 3:          | nil | nil | nil | nil |
-    ///     //Row 4:          |  4  | nil | nil | nil |
-    ///     //Row 4:          |  1  | nil | nil | nil |
+    ///     //Row 1:          | 4 | nil | 4 | nil | 2 |
+    ///     //Row 2:          | 4 | nil | 4 | nil | 2 |
+    ///     //Row 3:          | 4 | nil | 4 | nil | 2 |
+    ///     //Row 4:          | 4 | nil | 4 | nil | 2 |
+    ///     //Row 5:          | 4 | nil | 4 | nil | 2 |
     ///
-    /// - Parameter direction: The direction in which the elements will move
-    /// - Returns: Execution status. If there is nothing to move, false will be returned
+    /// - Parameter direction: The direction in which adjacent elements will be summed
+    /// - Returns: Coordinates of the elements that were summedned
     @discardableResult
-    public mutating func summationIdentical(_ direction: Matrix.Direction) -> Bool {
+    public mutating func sumIdentical(in direction: Matrix.Direction) -> [(from: Int, to: Int)] {
         //General process logic
         func sharedProcess(
             condition: ((x: Int, y: Int)) -> Bool,
             nextIndex: (_ index: Int) -> Int){
-                self.iterateRecursively { x, y, index, out in
+                self.iterate { x, y, index in
                     if self.storage[index] != nil && condition((x, y)) {
                         let nextIndex = nextIndex(index)
                         if combineAndClear(from: nextIndex, to: index) {
-                            self.rearrangement(in: direction)
-                            out = false
-                            isSuccess = true
+                            output.append((nextIndex, index))
                         }
                     }
                 }
@@ -89,10 +87,8 @@ extension Matrix where Element: SignedNumeric {
             }
             return false
         }
-        //Pre-movement
-        self.rearrangement(in: direction)
         //Return value
-        var isSuccess: Bool = false
+        var output: [(from: Int, to: Int)] = []
         switch direction {
         case .upward:
             sharedProcess(condition: { $0.y < self.row }, nextIndex: { $0 + self.column })
@@ -103,7 +99,7 @@ extension Matrix where Element: SignedNumeric {
         case .right:
             sharedProcess(condition: { $0.x > 1 }, nextIndex: { $0 - 1 })
         }
-        return isSuccess
+        return output
     }
     
     
