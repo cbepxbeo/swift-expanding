1b265fd43e497f78521d07c059c447cbfe420b40
Egor Olegovich <cbepxbeo@mail.ru>
2024-01-13 19:42:19 +0300

    Update ExpandingMatrixOperatorsTests.swift

--- Tests/ExpandingTests/Matrix/ExpandingMatrixOperatorsTests.swift
+++ Tests/ExpandingTests/Matrix/ExpandingMatrixOperatorsTests.swift
@@ -23,46 +23,32 @@ import XCTest
 @testable import Expanding
 
 final class ExpandingMatrixOperatorsTests: XCTestCase {
+    
+    let arrayA = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+    let arrayB = [5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8, 5, 6, 7, 8]
+    let arrayWithOptional = [1, nil, 2, nil, 3, nil, 4, nil, 1, nil, 2, nil, 3, nil, 4, nil]
+    let arrayOnlyOptional: [Int?] = .init(repeating: nil, count: 16)
+    let count = 4
+    
     func testOperatorPlus() throws {
-        let arrayA = [
-            1, 2, 3, 4,
-            1, 2, 3, 4,
-            1, 2, 3, 4,
-        ]
-        let arrayB = [
-            5, 6, 7, 8,
-            5, 6, 7, 8,
-            5, 6, 7, 8,
-        ]
-        
-        let arrayOutput = [
-            6, 8, 10, 12,
-            6, 8, 10, 12,
-            6, 8, 10, 12,
-        ]
-        
-        let matrixA = Matrix(column: 4, row: 3, storage: arrayA)
-        let matrixB = Matrix(column: 4, row: 3, storage: arrayB)
-        let matrixOutput = Matrix(column: 4, row: 3, storage: arrayOutput)
+        let arrayOutput = (0...15).map{ self.arrayA[$0] + self.arrayB[$0] }
+        let matrixA = Matrix(column: self.count, row: self.count, storage: self.arrayA)
+        let matrixB = Matrix(column: self.count, row: self.count, storage: self.arrayB)
+        let matrixOutput = Matrix(column: self.count, row: self.count, storage: arrayOutput)
         
         let output = try matrixA + matrixB
         XCTAssert(output == matrixOutput)
     }
     
     func testOperatorPlusWithOptionalValues() throws {
-        let array = [
-            1, 2, 3, 4,
-            1, nil, 3, nil,
-            1, 2, 3, 4,
-        ]
-
-        let matrixA = try Matrix(column: 4, row: 3, element: Int.self)
-        let matrixB = Matrix(column: 4, row: 3, storage: array)
-                
+        let matrixA = Matrix(column: self.count, row: self.count, storage: self.arrayWithOptional)
+        let matrixB = Matrix(column: self.count, row: self.count, storage: self.arrayOnlyOptional)
+    
         let outputA = try matrixA + matrixB
         let outputB = try matrixB + matrixA
-        XCTAssert(outputA == matrixB)
-        XCTAssert(outputB == matrixB)
+    
+        XCTAssert(outputA == matrixA)
+        XCTAssert(outputB == matrixA)
     }
     
     func testOperatorPlusWithError() throws {
@@ -77,4 +63,39 @@ final class ExpandingMatrixOperatorsTests: XCTestCase {
         }
         XCTAssert(output)
     }
+    
+    func testOperatorMultiplication() throws {
+        let arrayOutput = (0...15).map{ self.arrayA[$0] * self.arrayB[$0] }
+        let matrixA = Matrix(column: self.count, row: self.count, storage: self.arrayA)
+        let matrixB = Matrix(column: self.count, row: self.count, storage: self.arrayB)
+        let matrixOutput = Matrix(column: self.count, row: self.count, storage: arrayOutput)
+        
+        let output = try matrixA * matrixB
+        XCTAssert(output == matrixOutput)
+    }
+    
+    func testOperatorMultiplicationWithOptionalValues() throws {
+        let matrixA = Matrix(column: self.count, row: self.count, storage: self.arrayWithOptional)
+        let matrixB = Matrix(column: self.count, row: self.count, storage: self.arrayOnlyOptional)
+    
+        let outputA = try matrixA * matrixB
+        let outputB = try matrixB * matrixA
+
+        XCTAssert(outputA == matrixA)
+        XCTAssert(outputB == matrixA)
+    }
+    
+    func testOperatorMultiplicationWithError() throws {
+        let matrixA = try Matrix(column: 5, row: 5, element: Int.self)
+        let matrixB = try Matrix(column: 4, row: 4, element: Int.self)
+
+        var output: Bool = false
+        do {
+            let _ = try matrixA * matrixB
+        } catch let error as MatrixError where error == .matricesNotIdenticalStructure {
+            output = true
+        }
+        XCTAssert(output)
+    }
+    
 }
