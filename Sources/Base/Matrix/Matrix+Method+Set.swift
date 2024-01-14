59b5d87ff4b896f609946c14de23d29c501f9c4c
Egor Olegovich <cbepxbeo@mail.ru>
2024-01-14 21:51:03 +0300

    Update Matrix+Method+Set.swift
    doc

--- Sources/Base/Matrix/Matrix+Method+Set.swift
+++ Sources/Base/Matrix/Matrix+Method+Set.swift
@@ -26,8 +26,6 @@
 extension Matrix {
     /// Sets a new value at the specified coordinates
     ///
-    ///     var matrix = try! Matrix(column: 3, row: 3, defaultValue: 0)
-    ///
     ///     //Row 1:          | 0 | 0 | 0 |
     ///     //Row 2:          | 0 | 0 | 0 |
     ///     //Row 3:          | 0 | 0 | 0 |
@@ -41,7 +39,8 @@ extension Matrix {
     /// - Parameter value: New value
     /// - Parameter x: Coordinate corresponding to the position in the row
     /// - Parameter y: Coordinate corresponding to the position in the column
-    /// - Note: By calling a method in different threads on the same instance, you will not receive an exception, but the behavior will be undefined
+    /// - Note: By calling a method in different threads on the same instance,
+    /// you will not receive an exception, but the behavior will be undefined
     public mutating func set(_ value: Element, x xCoordinate: Int, y yCoordinate: Int) throws {
         var temp = self
         let index = try temp.index(x: xCoordinate, y: yCoordinate)
